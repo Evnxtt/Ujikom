44619518268fe31cfe45da066cd0f76743ab6d88
Venext <119793154+Evnxtt@users.noreply.github.com>
2023-03-06 08:42:25 +0700

    Delete fold directory

--- fold/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include "koneksi.php";
-    $id = $_GET['id'];
-    $ambilData = mysqli_query($koneksi, "DELETE FROM akun WHERE id='$id'");
-    echo "<meta http-equiv='refresh' content='1;url=http://localhost/23paskal/DAkun.php'>";
-?>
